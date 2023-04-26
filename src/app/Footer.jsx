81b4a9e7b6489974a8bb28345291ed1cfe34c5bd
DanX3N0 <danieldv283@gmail.com>
2023-04-25 23:45:21 -0600

    feat(common): v1.2.1

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -3,8 +3,8 @@ const Footer = () => {
    return (
       <div className="footerContainer">
          <div className="footer">
-            <p className="social">Social:</p>
-            <a href="https://github.com/DanX3N0">github</a>
+            <p className="social">Code of this project:</p>
+            <a href="https://github.com/DanX3N0/potafolio-js">github</a>
             <img src="https://th.bing.com/th/id/R.7a864f07681f187fb572468bfc949977?rik=3fUik6Pc6xTrHQ&pid=ImgRaw&r=0" alt="github" />
          </div>
       </div>
