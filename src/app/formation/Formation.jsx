9a06322501814b6ee4eb7e8dda8b1ce89428e805
DanX3N0 <danieldv283@gmail.com>
2023-04-26 00:15:56 -0600

    feat(common): v1.2.3

--- src/app/formation/Formation.jsx
+++ src/app/formation/Formation.jsx
@@ -8,25 +8,35 @@ export default function formation(){
             <p className='information'>Bachelor of Humanities.</p>
          </div>
 
+         <div className='line'></div>
+         
          <div className='formationContent'>
             <p className='date'>2020</p>
             <p className='information'>Application and acceptance in the University UATF.</p>
          </div>
       
+         <div className='line'></div>
+
          <div className='formationContent'>
             <p className='date'>2022</p>
             <p className='information'>Invitation for teaching assistant 1/2022.</p>
          </div>
+
+         <div className='line'></div>
 
          <div className='formationContent'>
             <p className='date'>2022</p>
             <p className='information'>Invitation for teaching assistant 2/2022.</p>
          </div>
 
+         <div className='line'></div>
+
          <div className='formationContent'>
             <p className='date'>2023</p>
             <p className='information'>Teaching assistant for the year 2023.</p>
          </div>
+
+         <div className='line'></div>
 
          <div className='formationContent'>
             <p className='date'>2024</p>
